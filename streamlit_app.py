123bc78f8b73d13bf3f7b72c9657133b6e61053f
NhungLNM <10323056@student.vgu.edu.vn>
2024-05-30 07:16:21 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,38 +12,31 @@ import cartopy.crs as ccrs
 import cartopy.feature as cfeature
 import plotly.express as px
 import streamlit as st
-import pandas as pd
 import os
-path = r"C:\Users\USER\Documents\athlete_events.xlsx"
 
-# Print current working directory
-print("Current Working Directory:", os.getcwd())
 
-# List files in the specified directory
-print("Files in Directory:", os.listdir('/mount/src/demo/'))
-
-# Define the path to the Excel file
-file_path = '/mount/src/Documents/athlete_events.xlsx'
-
-# Check if the file exists and read it
-if os.path.exists(file_path):
+# Read the Excel file
+file_path = r"C:\Users\admin\OneDrive\文档\GitHub\Python\Athlete_events.xlsx"
+try:
     athlete_events = pd.read_excel(file_path)
-    print("File read successfully.")
-else:
-    print(f"File not found: {file_path}")
+except FileNotFoundError as e:
+    st.error(f"File not found error: {e}")
+except Exception as e:
+    st.error(f"An error occurred: {e}")
 
-athlete_events = pd.read_excel(r'C:\Users\USER\Documents\athlete_events.xlsx')
+# Display the data
+st.write(athlete_events)
+import streamlit as st
+import pandas as pd
 
+# Define the URL of the Excel file on GitHub
+file_url = "https://github.com/kimduyenn/Python/raw/main/Athlete_events.xlsx"
 
+# Read the Excel file
+try:
+    athlete_events = pd.read_excel(file_url)
+except Exception as e:
+    st.error(f"An error occurred while reading the Excel file: {e}")
 
-# PLOT 1
-
-selected_sports = ["Athletics", "Badminton", "Boxing", "Cycling", "Gymnastics", "Swimming"]
-my_data = athlete_events[(athlete_events['Year'] == 2016) & (athlete_events['Sport'].isin(selected_sports))]
-
-sport_counts = my_data['Sport'].value_counts(normalize=True) * 100
-
-fig = px.pie(values=sport_counts, names=sport_counts.index, title='Distribution of Athletes in Selected Sports (2016)', hole=0.1)
-fig.update_traces(textposition='inside', textinfo='percent+label')
-
-st.plotly_chart(fig)
+# Display the data
+st.write(athlete_events)
